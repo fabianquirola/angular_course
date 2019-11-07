e1406c3b49614af55de0faa92a4db69de375da47
Fabian Quirola <fabian.quirola@kfc.com.ec>
2019-11-06 20:20:10 -0500

    first commit

---
+++ projects/bookshop-website/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { HeaderComponent } from './global/header/header.component';
+import { LogoComponent } from './global/logo/logo.component';
+import { SearchComponent } from './global/search/search.component';
+import { UtilNavComponent } from './global/util-nav/util-nav.component';
+import { MenuComponent } from './global/menu/menu.component';
+import { FooterComponent } from './global/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    SearchComponent,
+    UtilNavComponent,
+    MenuComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
