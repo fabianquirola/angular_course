def64794f5c16703b872a219ef9cc6caff395eba
Fabian Quirola <fabian.quirola@kfc.com.ec>
2019-11-07 20:45:20 -0500

    clase3 fin

---
+++ projects/bookshop-website/src/app/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { HeroComponent } from './hero/hero.component';
+import { ShopFeaturesComponent } from './shop-features/shop-features.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    HeroComponent,
+    ShopFeaturesComponent
+
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    FontAwesomeModule
+  ]
+})
+export class HomeModule { }
